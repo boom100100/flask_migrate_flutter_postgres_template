d8b0b7e1b0236d68a1008bfcdcb33686cde692b1
bernadette <bsd231@nyu.edu>
2025-06-19 15:54:15 -0400

    added cleanup

--- setup.sh
+++ setup.sh
@@ -1,19 +1,18 @@
 # create flutter app
 flutter create flutter_app
-cp Dockerfile.flutter ./flutter_app/Dockerfile
-cp -f widget_test.dart ./flutter_app/test/widget_test.dart
-cp -f main.dart ./flutter_app/lib/main.dart
-cp -f pubspec.yaml ./flutter_app/pubspec.yaml
+mv Dockerfile.flutter ./flutter_app/Dockerfile
+mv -f widget_test.dart ./flutter_app/test/widget_test.dart
+mv -f main.dart ./flutter_app/lib/main.dart
+mv -f pubspec.yaml ./flutter_app/pubspec.yaml
 
 # create react app
 npx create-react-app react_app
-# TBH, you should probably use a framework.
+# TBH, using a framework around react would be better.
 #  npx create-next-app@latest
 cp Dockerfile.react_dev ./react_app/Dockerfile
-cp -f App.js ./react_app/src/App.js
+mv Dockerfile.react_dev ./react_app/Dockerfile.react_dev
+mv Dockerfile.react_prod ./react_app/Dockerfile.react_prod
+mv -f App.js ./react_app/src/App.js
 
 # compose and run docker containers
 docker-compose build; docker-compose up -d
-
-# rewrite .gitignore
-echo ".DS_Store" > .gitignore
